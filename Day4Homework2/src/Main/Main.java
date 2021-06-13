1e9133f4943bbdbd6f5acd9d386e65b98f868582
mcancankaya <83570496+mcancankaya@users.noreply.github.com>
2021-06-14 02:53:14 +0300

    Update Main.java

--- Day4Homework2/src/Main/Main.java
+++ Day4Homework2/src/Main/Main.java
@@ -14,11 +14,11 @@ import Entities.Gamer;
 public class Main {
 
 	public static void main(String[] args) {
-		Gamer gamer= new Gamer(1, "Mahmut Can", "Çankaya","10304800770",LocalDate.of(1999, 8, 25));
+		Gamer gamer= new Gamer(1, "ad", "soyad","11111111111",LocalDate.of(1111, 1, 1));
 		
 		Game game = new Game(1,"CS:GO","fps Oyunu",200);
 		
-		Campaign campaign= new Campaign(1,"Yaz Kampanyasý","Yaza özel Tüm oyunlarda %10 indirim",10);
+		Campaign campaign= new Campaign(1,"Yaz KampanyasÃ½","Yaza Ã¶zel TÃ¼m oyunlarda %10 indirim",10);
 		
 		
 		GamerManager gamerManager= new GamerManager(new MernisServiceAdapter());
